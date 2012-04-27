4a0db0142ca6cd1cf0ec689b4e9714e4b497b605
Alex Leigh <leigh@alexleigh.me>
2012-04-27 08:33:40 -0400

    Networking and threading.

--- src/com/atomatica/spybot/SpybotActivity.java
+++ src/com/atomatica/spybot/SpybotActivity.java
@@ -1,84 +1,104 @@
 package com.atomatica.spybot;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.*;
 import java.security.InvalidParameterException;
-import java.util.Arrays;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 import android_serialport_api.SerialPort;
 
 public class SpybotActivity extends Activity {
+    public static final byte maintain = 0;
+    public static final byte led1 = 10;
+    public static final byte led2 = 11;
+    public static final byte led3 = 12;
+    public static final byte servo = 20;
+    public static final byte rightM = 21;
+    public static final byte leftM = 22;
+    
 	private SpybotApplication mApplication;
-	private SerialPort mSerialPort;
-    private InputStream mInputStream;
-	private OutputStream mOutputStream;
-	private ReceivingThread mReceivingThread;
-    private SendingThread mSendingThread;
 	
-    private EditText mReception;
+	private SerialPort mSerialPort;
+    private InputStream mSerialInput;
+	private OutputStream mSerialOutput;
+	private SerialReceivingThread mSerialReceivingThread;
+    private SerialSendingThread mSerialSendingThread;
+	
+    private Socket mNetworkPort;
+    private ObjectInputStream mNetworkInput;
+    private ObjectOutputStream mNetworkOutput;
+    private NetworkReceivingThread mNetworkReceivingThread;
     
-    byte[] mBuffer;
-    byte maintain = 0;
-    byte led1 = 10;
-    byte led2 = 11;
-    byte led3 = 12;
-    byte servo = 20;
-    byte rightM = 21;
-    byte leftM = 22;
+    private TextView mSerialText;
+    private TextView mNetworkText;
     
-	private class ReceivingThread extends Thread {
+	private class SerialReceivingThread extends Thread {
 		@Override
 		public void run() {
-			super.run();
 			while(!isInterrupted()) {
-				try {
-					byte[] buffer = new byte[64];
-					if (mInputStream == null) {
-					    return;
-					}
-					int size = mInputStream.read(buffer);
-					if (size > 0) {
-						onDataReceived(buffer, size);
-					}
-				}
-				catch (IOException e) {
-					e.printStackTrace();
-					return;
-				}
+                if (mSerialInput != null) {
+                    try {
+                        byte[] buffer = new byte[64];
+                        int size = mSerialInput.read(buffer);
+                        if (size > 0) {
+                            onSerialDataReceived(buffer, size);
+                        }
+                    }
+				    catch (IOException e) {
+				        e.printStackTrace();
+				        return;
+				    }
+			    }
 			}
 		}
 	}
 	
-    private class SendingThread extends Thread {
+    private class SerialSendingThread extends Thread {
         @Override
         public void run() {
-            /*while (!isInterrupted()) {
-                try {
-                    if (mOutputStream != null) {
-                        mOutputStream.write(mBuffer);
+            while (!isInterrupted()) {
+                if (mSerialOutput != null) {
+                    try {
+                        mSerialOutput.write(maintain);
+                        mSerialOutput.write((byte)0x0);
                     }
-                    else {
+                    catch (IOException e) {
+                        e.printStackTrace();
                         return;
                     }
                 }
-                catch (IOException e) {
-                    e.printStackTrace();
-                    return;
+            }
+        }
+    }
+    
+    private class NetworkReceivingThread extends Thread {
+        @Override
+        public void run() {
+            while(!isInterrupted()) {
+                if (mNetworkInput != null) {
+                    try {
+                        String message = (String)mNetworkInput.readObject();
+                        onNetworkDataReceived(message);
+                    }
+                    
+                    catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    
+                    catch (IOException e) {
+                        e.printStackTrace();
+                        return;
+                    }
                 }
-            }*/
+            }
         }
     }
 
@@ -102,70 +122,60 @@ public class SpybotActivity extends Activity {
         
         mApplication = (SpybotApplication)getApplication();
         
-        mBuffer = new byte[2];
-        mBuffer[0] = maintain;
-        mBuffer[1] = maintain;
-        
+        // setup serial connection
         try {
             mSerialPort = mApplication.getSerialPort();
-            mOutputStream = mSerialPort.getOutputStream();
-            mInputStream = mSerialPort.getInputStream();
+            mSerialInput = mSerialPort.getInputStream();
+            mSerialOutput = mSerialPort.getOutputStream();
 
-            // create a receiving thread
-            mReceivingThread = new ReceivingThread();
-            mReceivingThread.start();
+            // create a serial receiving thread
+            mSerialReceivingThread = new SerialReceivingThread();
+            mSerialReceivingThread.start();
             
-            // create a sending thread
-            mSendingThread = new SendingThread();
-            mSendingThread.start();
+            // create a serial sending thread
+            mSerialSendingThread = new SerialSendingThread();
+            mSerialSendingThread.start();
         }
         
         catch (SecurityException e) {
             DisplayError(R.string.error_security);
+        }
+
+        catch (InvalidParameterException e) {
+            DisplayError(R.string.error_configuration);
         }
         
         catch (IOException e) {
             DisplayError(R.string.error_unknown);
         }
         
-        catch (InvalidParameterException e) {
-            DisplayError(R.string.error_configuration);
+        // setup network connection
+        try {
+            mNetworkPort = new Socket(InetAddress.getByName("atomatica.com"), 9103);
+            mNetworkInput = new ObjectInputStream(mNetworkPort.getInputStream());
+            mNetworkOutput = new ObjectOutputStream(mNetworkPort.getOutputStream());
         }
         
-        mReception = (EditText)findViewById(R.id.EditTextReception);
+        // server closed connection
+        catch (EOFException e) {
+            DisplayError(R.string.error_unknown);
+        }
         
-        EditText Emission = (EditText)findViewById(R.id.EditTextEmission);
-        Emission.setOnEditorActionListener(new OnEditorActionListener() {
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                int i;
-                CharSequence t = v.getText();
-                char[] text = new char[t.length()];
-                for (i=0; i<t.length(); i++) {
-                    text[i] = t.charAt(i);
-                }
-                try {
-                    mOutputStream.write(new String(text).getBytes());
-                    mOutputStream.write('\n');
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            }
-        });
+        catch (IOException e) {
+            DisplayError(R.string.error_unknown);
+        }
+        
+        mSerialText = (TextView)findViewById(R.id.serial_text);
+        mNetworkText = (TextView)findViewById(R.id.network_text);
         
         final Button led1Button = (Button)findViewById(R.id.led1_button);
         led1Button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mBuffer[0] = led2;
-                mBuffer[1] = (byte)0xaa;
-                for (int i = 0; i < 100; i++) {
+                if (mSerialOutput != null) {
                     try {
-                        if (mOutputStream != null) {
-                            mOutputStream.write(mBuffer);
-                        }
-                        else {
-                            return;
-                        }
+                        //mSerialOutput.write(led1);
+                        //mSerialOutput.write((byte)0xaa);
+                        mNetworkOutput.writeObject("hello");
                     }
                     catch (IOException e) {
                         e.printStackTrace();
@@ -176,11 +186,21 @@ public class SpybotActivity extends Activity {
         });
     }
     
-    protected void onDataReceived(final byte[] buffer, final int size) {
+    protected void onSerialDataReceived(final byte[] buffer, final int size) {
         runOnUiThread(new Runnable() {
             public void run() {
-                if (mReception != null) {
-                    mReception.append(new String(buffer, 0, size));
+                if (mSerialText != null) {
+                    mSerialText.setText(new String(buffer, 0, size));
+                }
+            }
+        });
+    }
+    
+    protected void onNetworkDataReceived(final String message) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                if (mNetworkText != null) {
+                    mNetworkText.setText(message);
                 }
             }
         });
@@ -188,16 +208,28 @@ public class SpybotActivity extends Activity {
     
 	@Override
 	protected void onDestroy() {
-		if (mReceivingThread != null) {
-			mReceivingThread.interrupt();
+	    // close network connection
+	    try {
+	        mNetworkInput.close();
+	        mNetworkOutput.close();
+	        mNetworkPort.close();
+	    }
+	    catch (IOException e) {
+            DisplayError(R.string.error_unknown);
+	    }
+	    
+	    // close serial connection
+		if (mSerialReceivingThread != null) {
+			mSerialReceivingThread.interrupt();
 		}
 
-        if (mSendingThread != null) {
-            mSendingThread.interrupt();
+        if (mSerialSendingThread != null) {
+            mSerialSendingThread.interrupt();
         }
         
 		mApplication.closeSerialPort();
 		mSerialPort = null;
+		
 		super.onDestroy();
 	}
 }
